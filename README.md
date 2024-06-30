7c02ddb72acf10b9dd1231dfc7aaa8e994e301d5
Yasir-moha <yasir.mohammed@student.moringaschool.com>
2024-06-30 23:38:13 +0300

    completed

---
+++ README.md
@@ -0,0 +1,11 @@
+Week 1 Code Challenge
+yasir mohamed
+
+Student Grade Generator
+This program assigns grades to students depending on the marks that have been input by the user when prompted.
+
+Speed Detector
+The program checks the speed of car and outputs consequences depending on the number of demerit points assigned when the speed is above 70km/h.And if the user has more than 12 demerit points assigned the license is suspended .
+
+Net Salary Calculator
+Program calculates the net salary of an ind
---
+++ speeddetector.js
@@ -0,0 +1,19 @@
+// Prompt the user for input
+
+
+function calculateDemeritPoints() {
+    const speed = prompt(" carspeed:");
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+  
+    if (speed <= speedLimit) {
+        console.log("OK");
+        window.alert('OK')
+      //   return; 
+    }else {
+    const excessSpeed = speed - speedLimit;
+    const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
+  
+    if (demeritPoints <= 12) {
+      window.alert(`You have ${demeritPoints} demerit point(s)
+      }
